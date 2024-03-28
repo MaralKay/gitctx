420619b01a7066c7625247201cea79d67562fada
MaralKay <maralkou93@gmail.com>
2024-03-28 15:14:58 +0100

    - add migrate context command

---
+++ cmd/migrate-context.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func migrateCurrentContextFile(currentContextPath string) {
+
+	// Read the content of the file
+	content, err := os.ReadFile(currentContextPath)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		os.Exit(1)
+	}
+
+	// Modify the content by adding "global: " in front of the existing content
+	newContent := []byte("global: " + strings.TrimSpace(string(content)))
+
+	// Write the modified content back to the file
+	err = os.WriteFile(currentContextPath, newContent, 0644)
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Context file migrated successfully.")
+}
